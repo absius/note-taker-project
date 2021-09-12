7d3c5f4c2edd2550e4b1c76e9246b8c189efa58c
Patrick Abernathy <pat.abs@gmail.com>
2021-09-12 14:04:47 -0500

    core functionality

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const path = require('path');
 const fs = require('fs');
 const app = express();
 const PORT = 3001;
-
+const db = require("./db/db.json");
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    console.log('p ' +__dirname);
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
@@ -17,7 +17,32 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
+  app.get('/api/notes', (req, res) => {
+    return res.json(db);
+  });
 
+  app.post('/api/notes', (req, res) => {
+    req.body.id = db.length + 1;
+     db.push(req.body);
+     fs.writeFile('./db/db.json', JSON.stringify(db), function(){
+      console.log("Added to file");
+  });
+    res.json(db);
+  });
+
+  app.delete('/api/notes/:id', (req, res) => {
+    const newArray = db;
+    console.log(req.params.id);
+    for(let i=0;i<newArray.length;i++){
+      if (newArray[i].id == req.params.id){
+        newArray.splice(i,1);
+      }
+    }
+    fs.writeFile('./db/db.json', JSON.stringify(newArray), function(){
+      console.log("Removed from file");
+  });
+   res.json(newArray);
+ });
 
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
