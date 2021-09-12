8524ce89b0a815c9574aa2e8c8455d1754ea9f71
Patrick Abernathy <pat.abs@gmail.com>
2021-09-12 13:04:11 -0500

    working pages

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const path = require('path');
-
+const fs = require('fs');
 const app = express();
 const PORT = 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
 app.get('/', (req, res) => {
     console.log('p ' +__dirname);
-    res.sendFile(path.join(__dirname, '../../index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
   });
 
   app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
   });
 
-  app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
 
-  app.post('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-  });
 
   app.listen(PORT, () => {
     console.log(`App listening on PORT ${PORT}`);
